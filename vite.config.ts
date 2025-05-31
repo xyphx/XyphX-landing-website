50da982071212218806ac31230fdcc17b8a64b97
S SIDHARTH <92774572+callmesidhu@users.noreply.github.com>
2025-05-31 22:52:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/', io)
+  base: '/', 
   server: {
     host: "::",
     port: 8080,
